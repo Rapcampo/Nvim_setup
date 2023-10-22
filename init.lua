5f90d6ce2f32b1c489ac609cf5d1cf69da793411
Raphael Vieira <142260510+Rapcampo@users.noreply.github.com>
2023-10-23 00:14:44 +0100

    added vim settings

--- init.lua
+++ init.lua
@@ -45,6 +45,26 @@ vim.g.maplocalleader = ' '
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.g.moonflyTransparent = true
 
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "80"
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
@@ -157,7 +177,7 @@ require('lazy').setup({
     'bluz71/vim-moonfly-colors',
        priority = 1000,
     config = function()
-      --vim.opt.termguicolors = true
+      vim.opt.termguicolors = true
       vim.cmd.colorscheme 'moonfly'
       --vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
       --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
